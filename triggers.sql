cd0186ff73a8a7d80c9f3decbb864e4b5a6e1621
Spiros Tzokas <el21029@mail.ntua.gr>
2026-04-01 18:46:12 +0300

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,15 @@
+CREATE TRIGGER supervisor_nurse_check
+BEFORE INSERT ON nurse
+FOR EACH ROW
+
+BEGIN 
+    DECLARE supervisor_grade INT;
+
+    IF NEW.supervisor_nurse_id IS NOT NULL THEN             -- NEW is the new row inserted into the nurse table.
+        SELECT nurse_grade_id INTO supervisor_grade FROM nurse WHERE nurse_id = NEW.supervisor_nurse_id;
+
+        IF supervisor_grade <> 1 OR supervisor_grade IS NULL THEN 
+            SIGNAL_SQLSTATE '45000' SET MESSAGE_TEXT = 'Supervisor nurse must have grade 1';
+        END IF;
+    END IF;
+END;
