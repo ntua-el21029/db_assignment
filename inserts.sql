1863376d23e09a51ef173f62627b2c08e55e6095
Spiros Tzokas <el21029@mail.ntua.gr>
2026-04-15 17:25:27 +0300

    upd inserts

--- inserts.sql
+++ inserts.sql
@@ -6,6 +6,14 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (ken_code, ken_description, base_cost, mdn_days);
 
+LOAD DATA INFILE 'icd10_3digit_categories.csv' 
+INTO TABLE ICD10_codes 
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(icd_description, icd_category);
+
 LOAD DATA INFILE 'medicines_table_import.csv' 
 INTO TABLE medicines 
 CHARACTER SET utf8mb4
@@ -13,3 +21,4 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (medication_name, medication_route, medication_auth_country, medication_auth_holder, medication_file_location, medication_email, medication_number);
+
