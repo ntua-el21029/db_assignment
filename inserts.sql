5fab0f61b9e11409354e70c2780b77ffad7271ca
ntua-el21870 <el21870@mail.ntua.gr>
2026-04-15 17:16:31 +0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -6,3 +6,12 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (ken_code, ken_description, base_cost, mdn_days);
 na to balv sta inserts ?
+
+
+LOAD DATA INFILE 'medicines_table_import.csv' 
+INTO TABLE medicines 
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(medication_name, medication_route, medication_auth_country, medication_auth_holder, medication_file_location, medication_email, medication_number);
