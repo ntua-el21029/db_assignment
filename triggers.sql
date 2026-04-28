9ed177511e555490f666b369a12da4284f715a90
Spiros Tzokas <el21029@mail.ntua.gr>
2026-04-28 18:01:11 +0300

    new triggers

--- triggers.sql
+++ triggers.sql
@@ -1,14 +1,22 @@
 USE hospital_db;
 
+DROP TRIGGER IF EXISTS supervisor_nurse_check;
+DROP TRIGGER IF EXISTS check_medical_act_overlap;
+DROP TRIGGER IF EXISTS check_monthly_shift_limits;
+DROP TRIGGER IF EXISTS check_doctor_hierarchy;
+DROP TRIGGER IF EXISTS prevent_allergic_prescription;
+DROP TRIGGER IF EXISTS check_8_hour_rest;
+DROP TRIGGER IF EXISTS check_max_3_night_shifts;
+
 DELIMITER //
+
 CREATE TRIGGER supervisor_nurse_check
 BEFORE INSERT ON nurse
 FOR EACH ROW
-
 BEGIN 
     DECLARE supervisor_grade INT;
 
-    IF NEW.supervisor_nurse_id IS NOT NULL THEN             -- NEW is the new row inserted into the nurse table.
+    IF NEW.supervisor_nurse_id IS NOT NULL THEN
         SELECT nurse_grade_id INTO supervisor_grade FROM nurse WHERE nurse_id = NEW.supervisor_nurse_id;
 
         IF supervisor_grade <> 1 OR supervisor_grade IS NULL THEN 
@@ -22,7 +30,6 @@ CREATE TRIGGER check_medical_act_overlap
 BEFORE INSERT ON medical_act
 FOR EACH ROW
 BEGIN
-    -- elegxos gia idio dvmatio
     IF EXISTS (
         SELECT 1 FROM medical_act
         WHERE department_room_id = NEW.department_room_id
@@ -33,7 +40,6 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ΣΦΑΛΜΑ: Η αίθουσα χρησιμοποιείται ήδη σε άλλη επέμβαση αυτή την στιγμή';
     END IF;
 
-    -- idios kyrios xeiroyrgos
     IF EXISTS (
         SELECT 1 FROM medical_act
         WHERE main_surgeon_id = NEW.main_surgeon_id
@@ -45,7 +51,6 @@ BEGIN
 END;
 //
 
---ana kathgoria  15 iatroi , 20 noshleytes ,25 dioikhtikoi
 CREATE TRIGGER check_monthly_shift_limits
 BEFORE INSERT ON duty_schedule_team
 FOR EACH ROW
@@ -55,15 +60,12 @@ BEGIN
     DECLARE current_shifts INT;
     DECLARE emp_type VARCHAR(30);
 
-    -- briksv ton mhna kai etos ths neas bardias
     SELECT MONTH(date), YEAR(date) INTO shift_month, shift_year
     FROM duty_schedule WHERE duty_id = NEW.duty_id;
 
-    -- briskv to epaggelma toy ypallhloy poy thelv na balv 
     SELECT empl_type INTO emp_type
     FROM employee WHERE employee_id = NEW.employee_id;
 
-    --poses bardies exei o ypallhlos aytos mesa ston mhna 
     SELECT COUNT(*) INTO current_shifts
     FROM duty_schedule_team dst
     JOIN duty_schedule ds ON dst.duty_id = ds.duty_id
@@ -71,7 +73,6 @@ BEGIN
       AND MONTH(ds.date) = shift_month
       AND YEAR(ds.date) = shift_year;
 
-    -- elegxos me bash to eidos toy employee
     IF emp_type = 'doctor' AND current_shifts >= 15 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ΣΦΑΛΜΑ: Ο ιατρός έχει ήδη συμπληρώσει το όριο των 15 βαρδιών για αυτόν τον μήνα.';
     ELSEIF emp_type = 'nurse' AND current_shifts >= 20 THEN
@@ -82,26 +83,18 @@ BEGIN
 END;
 //
 
-USE hospital_db;
-
-DELIMITER //
-
---ierarxeia iatrvn kai epopteia
 CREATE TRIGGER check_doctor_hierarchy
 BEFORE INSERT ON doctor
 FOR EACH ROW
 BEGIN
-    -- eidikeyomenos , grade=1 exei epopth
     IF NEW.grade_id = 1 AND NEW.supervisor_doctor_id IS NULL THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ΣΦΑΛΜΑ: Ο Ειδικευόμενος ιατρός πρέπει υποχρεωτικά να έχει επόπτη.';
     END IF;
 
-    -- dieuthunths: grade_id = 4 den exei epopth
     IF NEW.grade_id = 4 AND NEW.supervisor_doctor_id IS NOT NULL THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ΣΦΑΛΜΑ: Ο Διευθυντής δεν επιτρέπεται να έχει επόπτη.';
     END IF;
 
-    -- apotroph kyklikhs epopteias 
     IF NEW.supervisor_doctor_id IS NOT NULL THEN
         IF EXISTS (
             SELECT 1 FROM doctor 
@@ -114,12 +107,10 @@ BEGIN
 END;
 //
 
--- gia tis allergies 
 CREATE TRIGGER prevent_allergic_prescription
 BEFORE INSERT ON medication_treatment
 FOR EACH ROW
 BEGIN
-    --  psaxnoyme taytish systatikvn farmakoy kai allergivn asthenh
     IF EXISTS (
         SELECT 1 
         FROM medicine_has_active_substance mhas
@@ -139,10 +130,8 @@ BEGIN
     DECLARE n_start DATETIME;
     DECLARE n_end DATETIME;
 
-    -- briskv thn hmeromhnia , vra enarkshs kai lhjhs ths neas bardias  
     SELECT 
         ADDTIME(CONVERT(ds.date, DATETIME), st.start_time),
-        -- an einai nyxterinh prepei na prosthesv mia mera
         IF(st.end_time < st.start_time, 
            ADDTIME(CONVERT(DATE_ADD(ds.date, INTERVAL 1 DAY), DATETIME), st.end_time), 
            ADDTIME(CONVERT(ds.date, DATETIME), st.end_time)
@@ -152,7 +141,6 @@ BEGIN
     JOIN shift_type st ON ds.shift_type_id = st.shift_type_id
     WHERE ds.duty_id = NEW.duty_id;
 
-    --  psaxnv an o ypallhlos exei hdh mia bardia poy sympiptei sto 8vro toy
     IF EXISTS (
         SELECT 1
         FROM duty_schedule_team dst
@@ -160,7 +148,6 @@ BEGIN
         JOIN shift_type st ON ds.shift_type_id = st.shift_type_id
         WHERE dst.employee_id = NEW.employee_id
         AND (
-            
             ADDTIME(CONVERT(ds.date, DATETIME), st.start_time) < DATE_ADD(n_end, INTERVAL 8 HOUR)
             AND 
             IF(st.end_time < st.start_time, 
@@ -173,33 +160,26 @@ BEGIN
     END IF;
 END;
 //
---oxi panv apo 3 synexomenes nyxterines bardies 
+
 CREATE TRIGGER check_max_3_night_shifts
 BEFORE INSERT ON duty_schedule_team
 FOR EACH ROW
 BEGIN
     DECLARE new_shift_desc VARCHAR(20);
     DECLARE new_date DATE;
-    
-   
-    DECLARE d_m1 INT DEFAULT 0; -- Ημέρα -1 (Χθες)
-    DECLARE d_m2 INT DEFAULT 0; -- Ημέρα -2 (Προχθές)
-    DECLARE d_m3 INT DEFAULT 0; -- Ημέρα -3
-    DECLARE d_p1 INT DEFAULT 0; -- Ημέρα +1 (Αύριο)
-    DECLARE d_p2 INT DEFAULT 0; -- Ημέρα +2 (Μεθαύριο)
-    DECLARE d_p3 INT DEFAULT 0; -- Ημέρα +3
+    DECLARE d_m1 INT DEFAULT 0;
+    DECLARE d_m2 INT DEFAULT 0;
+    DECLARE d_m3 INT DEFAULT 0;
+    DECLARE d_p1 INT DEFAULT 0;
+    DECLARE d_p2 INT DEFAULT 0;
+    DECLARE d_p3 INT DEFAULT 0;
 
-    
     SELECT st.shift_description, ds.date INTO new_shift_desc, new_date
     FROM duty_schedule ds
     JOIN shift_type st ON ds.shift_type_id = st.shift_type_id
     WHERE ds.duty_id = NEW.duty_id;
 
-    -- an h nea bardia einai night kanoyme elegxo
     IF new_shift_desc = 'Night' THEN
-        
-        -- Σαρώνουμε τις βάρδιες του υπαλλήλου στο παράθυρο των 7 ημερών (3 πριν, 3 μετά)
-        -- και βάζουμε "1" στα κουτάκια αν βρούμε νυχτερινή βάρδια εκείνη τη μέρα.
         SELECT 
             MAX(CASE WHEN ds.date = DATE_SUB(new_date, INTERVAL 1 DAY) THEN 1 ELSE 0 END),
             MAX(CASE WHEN ds.date = DATE_SUB(new_date, INTERVAL 2 DAY) THEN 1 ELSE 0 END),
@@ -215,20 +195,14 @@ BEGIN
           AND st.shift_description = 'Night'
           AND ds.date BETWEEN DATE_SUB(new_date, INTERVAL 3 DAY) AND DATE_ADD(new_date, INTERVAL 3 DAY);
 
-        
         IF (d_m3 = 1 AND d_m2 = 1 AND d_m1 = 1) OR
            (d_m2 = 1 AND d_m1 = 1 AND d_p1 = 1) OR
            (d_m1 = 1 AND d_p1 = 1 AND d_p2 = 1) OR
            (d_p1 = 1 AND d_p2 = 1 AND d_p3 = 1) THEN
-            
             SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ΣΦΑΛΜΑ: Απαγορεύεται η συμμετοχή σε περισσότερες από 3 συνεχόμενες νυχτερινές βάρδιες!';
         END IF;
-        
     END IF;
 END;
-
-
-
 //
 
 DELIMITER ;
