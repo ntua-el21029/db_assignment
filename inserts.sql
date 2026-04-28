0df991ae771a9ccd46aee6ff5b9507a0f8dafd8b
Spiros Tzokas <el21029@mail.ntua.gr>
2026-04-28 15:44:00 +0300

    inserts new

--- inserts.sql
+++ inserts.sql
@@ -1,40 +1,56 @@
-LOAD DATA INFILE 'ken.csv' 
+LOAD DATA INFILE 'C:/users/spiros/downloads/ken.csv' 
 INTO TABLE ken_system 
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-(ken_code, ken_description, base_cost, mdn_days);
+e, ken_de(ken_codscription, base_cost, mdn_days);
 
-LOAD DATA INFILE 'icd10_3digit.csv' 
-INTO TABLE ICD10_codes 
+LOAD DATA INFILE 'C:/users/spiros/downloads/icd10.csv'
+IGNORE INTO TABLE icd10_codes
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-(icd_id, icd_description, icd_category);
+(icd_id,icd_description, icd_category);
 
-LOAD DATA INFILE 'active_substances.csv' 
-INTO TABLE active_substances
+LOAD DATA INFILE 'C:/users/spiros/downloads/active_substances.csv'
+IGNORE INTO TABLE active_substances
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (substance_name, substance_description);
 
-LOAD DATA INFILE 'medicine_has_active_substance.csv' 
-INTO TABLE medicine_has_active_substance
+LOAD DATA INFILE 'C:/users/spiros/downloads/medicine_substances_junction.csv'
+IGNORE INTO TABLE medicine_has_active_substance
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (medication_id, active_substance_id);
 
-LOAD DATA INFILE 'medicines_table_import.csv' 
-INTO TABLE medicines 
+LOAD DATA INFILE 'C:/users/spiros/downloads/medicines.csv'
+IGNORE INTO TABLE medicines 
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 (medication_name, medication_route, medication_auth_country, medication_auth_holder, medication_file_location, medication_email, medication_number);
 
+
+LOAD DATA INFILE 'C:/users/spiros/downloads/medical_act_categories.csv'
+IGNORE INTO TABLE medical_act_categories
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(act_code, category, act_description);
+
+LOAD DATA INFILE 'C:/users/spiros/downloads/laboratory_exam_categories.csv'
+IGNORE INTO TABLE laboratory_exam_categories
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(exam_code, category, exam_description);
