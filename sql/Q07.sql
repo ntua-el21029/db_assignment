44d9d3565a23c0a570e1baf3e182f80cc9e39318
ntua-el21870 <el21870@mail.ntua.gr>
2026-05-17 17:18:33 +0300

    Refactor SQL query for allergic patients and medicines

--- sql/Q07.sql
+++ sql/Q07.sql
@@ -1,10 +1,14 @@
+-- Q07: Για κάθε δραστική ουσία, αριθμός ασθενών με δηλωμένη αλλεργία και
+-- αριθμός φαρμάκων που την περιέχουν, ταξινομημένα κατά συνολικό αριθμό
+-- αλλεργικών ασθενών (φθίνουσα).
+
 SELECT
-    asub.active_substance_id,
-    asub.substance_name,
-    COUNT(DISTINCT pha.patient_id)   AS allergic_patients_count,
-    COUNT(DISTINCT mhas.medication_id) AS medicines_containing_count
-FROM active_substances asub
-LEFT JOIN patient_has_allergy           pha  ON pha.active_substance_id  = asub.active_substance_id
-LEFT JOIN medicine_has_active_substance mhas ON mhas.active_substance_id = asub.active_substance_id
-GROUP BY asub.active_substance_id, asub.substance_name
-ORDER BY allergic_patients_count DESC, medicines_containing_count DESC, asub.substance_name;
+    asb.active_substance_id,
+    asb.substance_name,
+    COUNT(DISTINCT pha.patient_id)         AS num_allergic_patients,
+    COUNT(DISTINCT mhas.medication_id)     AS num_medicines
+FROM active_substances asb
+LEFT JOIN patient_has_allergy            pha  ON asb.active_substance_id = pha.active_substance_id
+LEFT JOIN medicine_has_active_substance  mhas ON asb.active_substance_id = mhas.active_substance_id
+GROUP BY asb.active_substance_id, asb.substance_name
+ORDER BY num_allergic_patients DESC, num_medicines DESC, asb.substance_name;
