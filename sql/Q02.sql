27b66688ccd1727c5f5ce62cb02119f85201b01c
Spiros Tzokas <el21029@mail.ntua.gr>
2026-05-19 16:26:46 +0300

    Update Q02.sql

--- sql/Q02.sql
+++ sql/Q02.sql
@@ -1,38 +1,30 @@
--- Q02: Για συγκεκριμένη ειδικότητα ιατρού, όλοι οι ιατροί που ανήκουν σε αυτήν,
--- με ένδειξη αν είχαν εφημερία το τρέχον έτος και πόσες χειρουργικές επεμβάσεις
--- εκτέλεσαν ως κύριοι χειρουργοί.
--- Παράμετρος: όνομα ειδικότητας (εδώ: 'Cardiology' – άλλαξέ το αν χρειάζεται)
-
 SELECT
-    d.doctor_id,
-    e.empl_first_name,
-    e.empl_last_name,
-    sp.specialty_name,
-    dg.grade_description,
+    doc.doctor_id AS DoctorID,
+    e.empl_first_name AS First_Name,
+    e.empl_last_name AS Last_Name,
+    sp.specialty_name AS Specialty,
     CASE
         WHEN EXISTS (
             SELECT 1
             FROM duty_schedule_team dst
             JOIN duty_schedule ds ON dst.duty_id = ds.duty_id
-            WHERE dst.employee_id = d.employee_id
+            WHERE dst.employee_id = doc.employee_id
               AND YEAR(ds.duty_date) = YEAR(CURRENT_DATE)
         ) THEN 'YES' ELSE 'NO'
-    END                                                     AS had_duty_current_year,
-    COUNT(DISTINCT ma.act_id)                               AS surgeries_as_main_surgeon
-FROM doctor d
-JOIN employee         e  ON d.employee_id = e.employee_id
-JOIN doctor_specialty sp ON d.specialty_id = sp.specialty_id
-JOIN doctor_grade     dg ON d.grade_id     = dg.grade_id
+    END                                                     AS Duty_this_year,
+    COUNT(DISTINCT ma.act_id)                               AS Surgeries_as_main
+    
+FROM doctor doc
+JOIN employee         e  ON doc.employee_id = e.employee_id
+JOIN doctor_specialty sp ON doc.specialty_id = sp.specialty_id
 LEFT JOIN medical_act ma
-       ON ma.main_surgeon_id = d.doctor_id
+       ON ma.main_surgeon_id = doc.doctor_id
       AND (LEFT(ma.medical_act_code, 1) = 'X'
            OR LEFT(ma.medical_act_code, 1) = 'Χ')
-WHERE sp.specialty_name = 'Cardiology'  -- <-- παράμετρος ειδικότητας
+WHERE sp.specialty_name = 'Surgery'  -- specialty variable
+
 GROUP BY
-    d.doctor_id,
-    e.empl_first_name,
-    e.empl_last_name,
-    sp.specialty_name,
-    dg.grade_description
-ORDER BY surgeries_as_main_surgeon DESC, e.empl_last_name;
+    doc.doctor_id
+ORDER BY
+    Surgeries_as_main DESC
 
