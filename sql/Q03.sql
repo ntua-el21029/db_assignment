d099df492ff91c1fbb0eeb53c6429da8adb302fe
Spiros Tzokas <el21029@mail.ntua.gr>
2026-05-19 18:48:12 +0300

    Update Q03.sql

--- sql/Q03.sql
+++ sql/Q03.sql
@@ -4,11 +4,12 @@ SELECT
     p.last_name AS Last_Name,
     hd.department_name AS Department,
     COUNT(h.hospitalization_id) AS Number_of_hospitalizations,
-    SUM(h.total_cost) AS Total_cost
+    SUM(h.total_cost+le.exam_cost) AS Total_cost
     
 FROM hospitalization h
 JOIN patient p ON p.patient_id = h.patient_id
 JOIN hospital_department hd ON h.department_id = hd.department_id
+JOIN laboratory_exams le ON le.hospitalization_id = h.hospitalization_id
 
 GROUP BY p.patient_id, hd.department_id
 HAVING COUNT(h.hospitalization_id) > 3 
